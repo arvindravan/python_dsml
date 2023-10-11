d91d54b9a187bf6e23318121a0b7308d9f878319
arvindravan <arvind_ravan@outlook.com>
2023-10-11 23:24:06 +0530

    preprocessing

---
+++ Data_Preprocessing/catconseperate.py
@@ -0,0 +1,19 @@
+def catconsep(df):
+    cat = list(df.columns[df.dtypes=="object"])
+    con = list(df.columns[df.dtypes!="object"])
+    return cat,con
+
+def replacer(df):
+    cat, con = catconsep(df)
+    for i in df.columns:
+        if i in cat:
+            md = df[i].mode()[0]
+            df[i] = df[i].fillna(md)
+        else:
+            mn=df[i].mean()
+            df[i]=df[i].fillna(mn)
+    print("Missing values replaced with mean and median !!!")
+
+def test():
+    return "test 1"
+        
