7c42dc237be7e447c561dcb2f35e121a9d9c7e56
arvindravan <arvind_ravan@outlook.com>
2023-10-12 19:00:16 +0530

    pre processing basics

--- Data_Preprocessing/catconseperate.py
+++ Data_Preprocessing/catconseperate.py
@@ -10,10 +10,8 @@ def replacer(df):
             md = df[i].mode()[0]
             df[i] = df[i].fillna(md)
         else:
-            mn=df[i].mean()
-            df[i]=df[i].fillna(mn)
-    print("Missing values replaced with mean and median !!!")
+            mn = df[i].mean()
+            df[i] = df[i].fillna(mn)
+    print('Missing Values replaced with mode and median')
 
-def test():
-    return "test 1"
         
